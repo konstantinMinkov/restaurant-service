99d802972cfef129c4422ea608e94f00a9379923
Konstantin <zakedystyle@gmail.com>
2016-12-25 13:50:46 +0200

    update UserRepository
    - add existByUsername method

--- src/main/java/ua/kpi/integrations/restaurant/repositories/UserRepository.java
+++ src/main/java/ua/kpi/integrations/restaurant/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package ua.kpi.integrations.restaurant.repositories;
 
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ua.kpi.integrations.restaurant.domain.User;
 
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findUserByUsername(String username);
+
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM User u WHERE u.username = :username")
+    boolean existsByUsername(@Param("username") String username);
 }
