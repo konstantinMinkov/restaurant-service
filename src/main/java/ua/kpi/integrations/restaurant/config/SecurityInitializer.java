2fba71cd9cf9530eb2b97d0e2ac3c87bd72296b7
vladtkachuk <tkachuk.v26@gmail.com>
2016-12-25 12:02:40 +0200

    add initializers
    - add SecurityInitializer
    - add WebAppInitializer

---
+++ src/main/java/ua/kpi/integrations/restaurant/config/SecurityInitializer.java
@@ -0,0 +1,9 @@
+package ua.kpi.integrations.restaurant.config;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+@Order(1)
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
