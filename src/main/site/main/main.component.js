11c60824bcb3c1fa651dce4e9b5595cbedffff1e
Konstantin <zakedystyle@gmail.com>
2016-12-25 11:40:25 +0200

    create main page component
    - add controller framework
    - add template framework

---
+++ src/main/site/main/main.component.js
@@ -0,0 +1,8 @@
+angular.module('restaurant-service').component('main', {
+    templateUrl: 'main.template.html',
+    controller: ['$scope', '$http',
+        function ($scope, $http) {
+
+        }
+    ]
+});
