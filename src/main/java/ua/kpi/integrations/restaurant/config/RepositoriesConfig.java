dd70de8e5b468415f59f7402a2c5458d742e3bcc
vladtkachuk <tkachuk.v26@gmail.com>
2016-12-26 19:35:33 +0200

    update schema creation to 'update'

--- src/main/java/ua/kpi/integrations/restaurant/config/RepositoriesConfig.java
+++ src/main/java/ua/kpi/integrations/restaurant/config/RepositoriesConfig.java
@@ -58,7 +58,7 @@ public class RepositoriesConfig {
     public Properties jpaProperties() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
         return properties;
